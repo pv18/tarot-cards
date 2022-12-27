d4cdd51b76bf54a689e31d378b9723bcb07443eb
pv18 <pv9998286620@yandex.ru>
2022-12-27 16:31:53 +0300

    connected antd

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from './components/App/App';
+import 'antd/dist/reset.css';
+import {BrowserRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
 
